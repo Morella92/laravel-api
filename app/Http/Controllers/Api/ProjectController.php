a51bfbd1e4128b90ffa7faba337838d747ab4a30
Morella92 <morena.piem@outlook.com>
2023-05-02 16:35:19 +0200

    FEAT with method to get typology names

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,7 +10,7 @@ class ProjectController extends Controller
 {
     public function index(){
 
-        $results = Project::all();
+        $results = Project::with('typology', 'technologies')->get();
 
         return response()->json([
             'success' => true,
