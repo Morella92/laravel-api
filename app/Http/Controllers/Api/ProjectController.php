20cc8c82b440c45581f629de83572ddc1fc103d0
Morella92 <morena.piem@outlook.com>
2023-05-04 14:55:11 +0200

    FEAT show route

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -17,4 +17,25 @@ class ProjectController extends Controller
             'results' => $results,
         ]);
     }
+    public function show($id)
+    {
+
+        $project = Project::where('id', $id)->first();
+
+        $relatedProject = project::where('id', '!=', $id)->orderBy('created_at', 'desc')->limit(3)->get();
+
+        $project->relatedProjects = $relatedProject;
+
+        if ($project) {
+            return response()->json([
+                'success' => true,
+                'project' => $project
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'error' => 'Nessun progetto trovato'
+            ]);
+        }
+    }
 }
